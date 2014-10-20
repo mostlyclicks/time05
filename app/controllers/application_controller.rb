d4d880fbc09c75ae24191665b93ee5ac3ca198b2
Carlos Interone <carlos@mostlyclicks.com>
2014-10-20 12:25:59 -0500

    add VXrs to home carousel

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -55,7 +55,9 @@ class ApplicationController < ActionController::Base
     @home_bikes = [
       {bike_url: '/framesets/skylon', bike_image: 'Skylon-AKTIV-Team_bike.png', bike_logo: 'SKYLON-LOGO-black.png', bike_headline: 'Skylon', bike_bg: 'home-bike-skylon-bg.jpg'},
       {bike_url: '/framesets/izon', bike_image: 'Izon-AKTIV-white-bike.png', bike_logo: 'IZON-LOGO-black.png', bike_headline: 'Izon', bike_bg: 'home-bike-izon-bg.jpg'},
-      {bike_url: '/framesets/fluidity', bike_image: 'Fluidity-AKTIV-black-bike.png', bike_logo: 'FLUIDITY-LOGO-black.png', bike_headline: 'Fluidity', bike_bg: 'home-bike-fluidity-bg.jpg'}
+      {bike_url: '/framesets/fluidity', bike_image: 'Fluidity-AKTIV-black-bike.png', bike_logo: 'FLUIDITY-LOGO-black.png', bike_headline: 'Fluidity', bike_bg: 'home-bike-fluidity-bg.jpg'},
+      {bike_url: '/framesets/limited-edition-bettini-vxrs', bike_image: 'Bettini-frame.png', bike_logo: 'VXRS-LOGO-black.png', bike_headline: 'Bettini Limited Edition VXrs', bike_bg: 'home-bike-bettini-bg.png'}
+
     ]
   end
 
